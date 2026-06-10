38680d64168a0bc330b64ac97551827b4e9ceab0
Liao Guan-Cheng <145754891+liao-guan-cheng@users.noreply.github.com>
2026-06-10 12:59:43 +0800

    Update BattleScene.tsx

--- client/src/components/BattleScene.tsx
+++ client/src/components/BattleScene.tsx
@@ -8,7 +8,7 @@ import Header from './Header';
 
 const GUARDIAN_IMG = 'https://d2xsxph8kpxj0f.cloudfront.net/310519663579132777/FbXy3vn6WfkCXwCrMUv9po/beyblade-guardian-new-2divrVh2tQ6XvR4F6zQEX3.webp';
 const BATTLE_VIDEO = '/manus-storage/battle-video_c7dacaf1.mp4';
-const LOGO_IMG = '/manus-storage/tainan-police-logo_6ffd133b.png';
+const LOGO_IMG = '/__manus__/logo.png';
 
 /**
  * 對決場景：
